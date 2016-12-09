06609ccbb11abad46df9083b48b9f0142431827c
Paulo Telles Filho <ptellesfilho@gmail.com>
2016-12-09 10:21:19 -0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -15,7 +15,7 @@ fi
 
 # General the email/password hash and remove evidence.
 if [ ! -z "${EMAIL}" ] && [ ! -z "${EMAILPASS}" ]; then
-    echo "[smtp.gmail.com]:587    ${EMAIL}:${EMAILPASS}" > /etc/postfix/sasl_passwd
+    echo "[smtp.office365.com]:587    ${EMAIL}:${EMAILPASS}" > /etc/postfix/sasl_passwd
     postmap /etc/postfix/sasl_passwd
     rm /etc/postfix/sasl_passwd
     echo "postfix EMAIL/EMAILPASS combo is setup."
